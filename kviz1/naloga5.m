cb24e239347251c12175d82efca8ec2c8f2de73d
Miha Avsec <mihavsec@gmail.com>
2018-11-12 19:45:10 +0100

    porpavljena navodila
    +pr simpleksni je spremenjen da je tm y tko kot v navodilih (u v navodilih je y)

--- kviz1/naloga5.m
+++ kviz1/naloga5.m
@@ -3,14 +3,15 @@
 c = [-3; -1; -3; 0; 0; 0];
 A = [2 1 1 1 0 0; 1 2 3 0 1 0; 2 2 1 0 0 1];
 b = [2;5;6];
-J = [4,5,6];
+%J = [4,5,6];
 % c = [0;0 ;0; 0; 0; 1; 1];
 % A = [0 1 2 0 0 1 0;-1 4 2 1 0 0 0; 2 1 3 0 -1 0 1];
 % b = [44;110;132];
 % J = [4 6 7];
 [x,vr,y,st] = simpleksMetoda( c,A,b,J);
 x
-linprog(c,[],[],A,b,zeros(length(c),1))
+vr
+[x1,vr] = linprog(c,[],[],A,b,zeros(length(c),1))
 
 %drug primer
 c = [-1 -1 0 0 0]';
@@ -18,4 +19,5 @@ A = [1 0 2 1 0;0 1 -1 0 1; 1 1 1 0 0];
 b = [1;1;2];
 [x,vr,y,st] = simpleksMetoda( c,A,b,[],100,2);
 x
-linprog(c,[],[],A,b,zeros(length(c),1))
+vr
+[x1,vr1] = linprog(c,[],[],A,b,zeros(length(c),1))
